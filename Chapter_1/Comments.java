9b6995e633d5a0792d91987b5c11fd0c842e64e5
JamesPiamonte-UCM <jamespiamonte43@gmail.com>
2025-09-17 08:27:54 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,10 @@
+public class Comments {
+    public static void main(String[] args) {
+
+        System.out.println("Single-line comment above this line." "/n // This is a single-line comment //");
+
+        System.out.println("Multi-line comment above this line.");
+
+        System.out.println("Documentation comment above this line.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Not because of who I am,");
+        System.out.println("But because of what You’ve done.");
+        System.out.println("Not because of what I’ve done,");
+        System.out.println("But because of who You are.");
+    }
+}
